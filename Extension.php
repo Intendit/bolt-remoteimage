327b02824cefc3caa6b649bc22e9cff0e701c1ca
Svante Richter <svante.richter@gmail.com>
2016-08-09 16:38:44 +0200

    Just commiting last changes before :fire:

--- Extension.php
+++ Extension.php
@@ -15,26 +15,31 @@ class Extension extends \Bolt\BaseExtension
             $this->addTwigFunction('remoteimage', 'remoteImage', array('is_variadic' => true));
         }
     }
-    
+
     public function remoteImage(array $args = array())
     {
         $defaults = array(
-              'url' => '',
+            'url' => '',
         );
+
         $args = array_merge($defaults, $args);
+
         if(empty($args['url'])){
             return "no url set";
         }
-        
+
         $parsedURL = parse_url($args['url']);
         $pathinfo = pathinfo($parsedURL['path']);
         
-        if(!in_array($pathinfo['extension'], $this->app['config']->get('general/accept_file_types'))){
+        if( !isset($pathinfo['extension']) || isset($pathinfo['extension']) && !in_array($pathinfo['extension'], $this->app['config']->get('general/accept_file_types'))){
             return "not an accepted file type";
         }
+
+        $returnurl = join(explode('/', $this->app['slugify']->slugify($parsedURL['host'] . $parsedURL['path'] . $parsedURL['query'])));
         
-        $path = $this->app['paths']['filespath'] . '/remotecache/' . join(explode('/', $parsedURL['path']));
-        $returnurl = 'remotecache/' . join(explode('/', $parsedURL['path']));
+        $returnurl = 'remotecache/' . hash('md5', $returnurl) . '.jpg';
+        
+        $path = $this->app['paths']['filespath'] . $returnurl ;
         
         if(!is_dir($this->app['paths']['filespath'] . '/remotecache')){
             mkdir($this->app['paths']['filespath'] . '/remotecache');
@@ -42,9 +47,13 @@ class Extension extends \Bolt\BaseExtension
         
         if (!is_file($path)) {
             $opts = [
-                'save_to' => $path,
+                'save_to' => $path
             ];
-            $this->app['guzzle.client']->get($args['url'], $opts)->getBody(true);
+            try {
+                $this->app['guzzle.client']->get($args['url'], $opts)->getBody(true);
+            } catch (\Exception $e) {
+                return ['error' =>  $e->getMessage()];
+            }
         }
         return $returnurl;
     }
